28f0631a280661bc20a5cf79407631d4419d34f3
Emil Loer <emil@koffietijd.net>
2010-12-22 12:17:42 +0100

    css sprite handling

--- jquery.ui.dial.js
+++ jquery.ui.dial.js
@@ -1,15 +1,19 @@
-(function($,undefined) {
+(function($, undefined) {
 	$.widget("ui.dial", {
 		options: {
 			min: -100,
 			max: 100,
-			unitsPerPixel: 10,
+			default: 25,
+			unitsPerPixel: 1,
+
+			numImages: 41,
+			imageWidth: 36
 		},
 
 		_create: function() {
-			this.value = 10;
-			this.origvalue = this.value;
-			
+			this._value = this.options.default;
+			this._origValue = this._value;
+
 			var self = this;
 
 			this.element.addClass("ui-dial");
@@ -18,20 +22,31 @@
 				helper: 'original',
 				scroll: false,
 				cursor: 'row-resize',
-			}).bind('drag',function(event,ui) {
-				self.value = self.origvalue + (ui.position.top - ui.originalPosition.top);
-				ui.position=ui.originalPosition;
-				console.log(self.value);
-			}).bind('dragstart',function(event,ui) {
-				self.origvalue = self.value;
+			}).bind('drag', function(event, ui) {
+				self._value = (self._origValue + (ui.position.top - ui.originalPosition.top) / -self.options.unitsPerPixel);
+				self._value = Math.max(self._value, self.options.min);
+				self._value = Math.min(self._value, self.options.max);
+				ui.position = ui.originalPosition;
+				self._update();
+			}).bind('dragstart', function(event, ui) {
+				self._origValue = self._value;
 			});
+
+			this._update();
 		},
 
 		destroy: function() {
-			$.Widget.prototype.destroy.apply(this,arguments);
+			$.Widget.prototype.destroy.apply(this, arguments);
 		},
 
 		value: function(newValue) {
+			this._value = this._origValue = 10;
+		},
+
+		_update: function() {
+			var pos = Math.round((this._value - this.options.min) / (this.options.max - this.options.min) * (this.options.numImages - 1)) * this.options.imageWidth;
+
+			this.element.css("background-position-x", "-" + pos + "px");
 		}
 	});
 })(jQuery);
